41c4c122fcaf6c910c4c58935ea2c3bf52e38bf6
daria <d4riyabel@yandex.ru>
2024-02-07 19:49:33 +0400

    Реклама с картинкой

--- index.py
+++ index.py
@@ -39,5 +39,49 @@ def image():
                 </html>"""
 
 
+@app.route('/promotion_image')
+def bootstrap():
+    prom = ["Человечество вырастает из детства.",
+            "Человечеству мала одна планета.",
+            "Мы сделаем обитаемыми безжизненные пока планеты.",
+            "И начнем с Марса!",
+            "Присоединяйся!"]
+
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+           
+                     <div class="alert alert-primary" role="alert">
+                     {prom[0]}
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      {prom[1]}
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      {prom[2]}
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      {prom[3]}
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                        {prom[4]}
+                    </div>
+
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
