f709b053df95877185137e403dac96708c8ddfa4
daria <d4riyabel@yandex.ru>
2024-02-07 19:40:22 +0400

    Изображение Марса

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -21,6 +21,22 @@ def countdown():
             "И начнем с Марса!",
             "Присоединяйся!"]
     return '</br>'.join(prom)
+
+
+@app.route('/image_mars')
+def image():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+                     <div>Вот она какая, красная планета.</div>
+                  </body>
+                </html>"""
 
 
 if __name__ == '__main__':
