3de85d7d94c46a56ac0968c9be6cb1cacc49e519
daria <d4riyabel@yandex.ru>
2024-02-07 19:23:35 +0400

    Initial commit

---
+++ index.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index1():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def countdown():
+    prom = ["Человечество вырастает из детства.",
+            "Человечеству мала одна планета.",
+            "Мы сделаем обитаемыми безжизненные пока планеты.",
+            "И начнем с Марса!",
+            "Присоединяйся!"]
+    return '</br>'.join(prom)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
