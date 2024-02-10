86530dddc431019a0673d750481e9c617dd4088b
daria <d4riyabel@yandex.ru>
2024-02-10 19:57:23 +0400

    Список профессий

--- index1.py
+++ index1.py
@@ -14,5 +14,16 @@ def training(prof):
     return render_template('train.html', prof=prof)
 
 
+@app.route('/list_prof/<listt>')
+def prof(listt):
+    proff = ["Инженер-исследователь",
+             "Инженер-строитель",
+             "Пилот",
+             "Метеоролог",
+             "Врач",
+             "Экзобиолог"]
+    return render_template('prof.html', listt=listt, proff=proff)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
